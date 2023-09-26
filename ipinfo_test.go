3668aed7d6e71a3ec6c1ed52d73a7de8059426ea
beyrak <beyraklnc@gmail.com>
2023-09-26 15:33:57 +0400

    Update data source and functionality

--- ipinfo_test.go
+++ ipinfo_test.go
@@ -1,28 +1,3 @@
 package main
 
-import "testing"
-
-func TestIsValidIp(t *testing.T) {
-	var table = []Table{
-		{"0.0.0.0", true},
-		{"192.168.0.0", true},
-		{"255.256.100.2", false},
-		{"-100.200.182.160", false},
-		{"300.200.182.160", false},
-		{"100.2333.182.160", false},
-		{"150.18.182,26", false},
-	}
-
-	for _, v := range table {
-		result := checkIpFormat(v.ip)
-
-		if result != v.isValid {
-			t.Errorf("Expection is %t, result is %t", v.isValid, result)
-		}
-	}
-}
-
-type Table struct {
-	ip      string
-	isValid bool
-}
+// write test for checkInput()
